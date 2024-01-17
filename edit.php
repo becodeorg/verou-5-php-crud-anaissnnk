f5ffcfc12ca168267600ff3362350817097f1646
Anaïs <aekyon.08@gmail.com>
2024-01-17 15:31:13 +0100

    Add edit page

---
+++ edit.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Let's update our quotes</title>
+</head>
+<body>
+
+<form method="POST">
+    <section class="quote-section">
+        <label for="quote">Quotes:</label>
+        <input type="text" id="quote" name="quote">
+    </section>
+
+    <input type="submit" name="submit">
+</form>
+    
+</body>
+</html>
